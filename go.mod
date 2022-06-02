c3a85ad398dd01bd4658c34f20f0a7a73cb44d71
notpop <k.tani.free@gmail.com>
2022-06-03 03:04:41 +0900

    create main.go parctice resize package

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/notpop/resize
 
 go 1.18
 
-require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+require github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	// "image/png"
+	"image/jpeg"
+	"log"
+	"os"
+	"github.com/nfnt/resize"
+)
+
+const RESIZE_WIDTH = 200
+const RESIZE_ASPECT_RATIO = 0
+
+const TEST_FILE_PATH = "targets/afn-s.jpeg"
+const RESIZED_TEST_FILE_PATH = "resized/afn-s.jpeg"
+
+func main() {
+	targetFile, error := os.Open(TEST_FILE_PATH)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	log.Print(targetFile)
+
+	image, error := jpeg.Decode(targetFile)
+	if error != nil {
+		log.Fatal(error)
+	}
+	targetFile.Close()
+
+	resizedImage := resize.Resize(RESIZE_WIDTH, RESIZE_ASPECT_RATIO, image, resize.Lanczos3)
+
+	output, error := os.Create(RESIZED_TEST_FILE_PATH)
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer output.Close()
+
+	jpeg.Encode(output, resizedImage, nil)
+}
